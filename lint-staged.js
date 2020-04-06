e25dfc87c12b93344ea3910b5baa541bb6c13df5
mcguiretj18 <mcguiretj18@gmail.com>
2020-04-06 12:17:27 -0400

    run husky

---
+++ lint-staged.js
@@ -0,0 +1,9 @@
+module.exports = {
+  linter: {
+    '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
+      'eslint --fix',
+      'prettier --write',
+      'git add .',
+    ],
+  },
+};
