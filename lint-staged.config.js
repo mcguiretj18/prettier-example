974274c428b4a03387ef258ac96c3fd65f3b675d
mcguiretj18 <mcguiretj18@gmail.com>
2020-04-06 12:51:04 -0400

    Fix eslint issues with .md files

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,8 @@
 module.exports = {
-  '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
+  '**/*.+(js|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
     'eslint --fix',
     'prettier --write',
     'git add',
   ],
+  '**/*.+(md)': ['prettier --write', 'git add'],
 };
